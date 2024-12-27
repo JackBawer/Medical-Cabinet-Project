62fe1acb81e6c781f2e1c964a0747198c309d878
Jack Bawer <jackb2507@gmail.com>
2024-12-27 12:37:12 +0100

    Added PatientFileService class

--- src/service/PatientFileService.java
+++ src/service/PatientFileService.java
@@ -1,43 +1,49 @@
 package service;
 
 import module.PatientFile;
-import module.Patient;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PatientFileService  {
-	
-	private List<PatientFile> patientFiles;
-	
-	public PatientFileService(List<PatientFile> patientFiles) {
-		this.patientFiles = new ArrayList<>();
-	}
-	
-	public List<PatientFile> getPatientFiles() {
-		return patientFiles;
-	}
+public class PatientFileService {
 
-	public void setPatientFiles(List<PatientFile> patientFiles) {
-		this.patientFiles = patientFiles;
-	}
+    private List<PatientFile> patientFiles;
 
-	void addPatient(Patient patient) {
-		return;
-	}
-	
-	void updatePatientInfo(Patient patient) {
-		return;
-	}
-	
-	void getPatientInfo(String lastName, String firstName) {
-		return;
-	}
-	void getPatientHistory(Patient patient) {
-		return;
-	}
-	
-	public String toString() {
-		return "This will be so long.";
-	}
- }
+    public PatientFileService(List<PatientFile> patientFiles) {
+        this.patientFiles = new ArrayList<>();
+  }
+
+    public List<PatientFile> getPatientFiles() {
+
+        return this.patientFiles;
+    }
+
+    public void setPatientFiles(List<PatientFile> patientFiles) {
+        this.patientFiles = patientFiles;
+    }
+
+    void addPatientFile(PatientFile patientFile) {
+        this.patientFiles.add(patientFile);
+    }
+
+    public PatientFile getPatientFile(String ID) {
+        for (PatientFile file: this.patientFiles) {
+            if (file.getPatient().getId().equals(ID)) {
+                return file;
+            }
+        }
+        System.out.println("No patient file found for patient with ID: " + ID);
+        return null;
+    }
+
+    public void updatePatientFile(PatientFile patientFile) {
+        for (PatientFile file: this.patientFiles) {
+            if (file.getPatient().getId().equals(patientFile.getPatient().getId())) {
+                file = patientFile;
+                return;
+            }
+        }
+
+        System.out.println("No patient file found for patient with ID: " + patientFile.getPatient().getId());
+    }
+}
